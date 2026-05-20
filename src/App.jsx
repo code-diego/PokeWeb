fbbe2b688a1bf7aa33dc353de57e995214a730da
Diego <thewolf2108@gmail.com>
2026-05-20 11:16:09 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Pokedex</h1>
+      </header>
+      <main>
+
+      </main>
+      <section id="center">
+        hola
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
