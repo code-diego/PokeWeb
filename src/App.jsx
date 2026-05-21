9f1f64d5f34a481688ed19e71d4dc0d4c486a800
Diego <thewolf2108@gmail.com>
2026-05-21 03:05:43 -0500

    First Architectire for mi website using API Rest

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import reactLogo from './assets/react.svg'
+import { use } from 'react'
 import './App.css'
+import { useState, useEffect } from 'react'
+import TarjetaPokemon from './components/TarjetaPokemon'
 
 function App() {
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then(data => {
+        const count = data.count
+        const pokes = data.results.map(pk => ({
+          name : pk.name,
+          id : pk.url.split('/')[6]
+        }))
+        setPokemons(pokes)
+      })
+
+  }, [])
+
 
   return (
-    <>
-      <header>
-        <h1>Pokedex</h1>
+    <div className='App'>
+      <header className='Header'>
+        <h1 className='Title'>Pokedex</h1>
       </header>
-      <main>
 
+      <main>
+        <TarjetaPokemon pokemons={pokemons} />
       </main>
-      <section id="center">
-        hola
-      </section>
-      <section id="spacer"></section>
-    </>
+    </div>
   )
 }
 
